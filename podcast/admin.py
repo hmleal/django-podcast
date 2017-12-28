c62c566a3e0dc9fc4c0a79a908fd3fc4007f5ec9
Henrique Leal <hm.leal@hotmail.com>
2017-12-28 14:56:34 +0000

    feature: Update channel models information

--- podcast/admin.py
+++ podcast/admin.py
@@ -3,6 +3,29 @@ from . import models
 
 
 class ChannelAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'slug', 'link', 'description')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': (
+                'language',
+                'copyright',
+                'managing_editor',
+                'web_master',
+                'pub_date',
+                'last_build_date',
+                'ttl',
+                'generator'
+            )
+        }),
+        ('iTunes', {
+            'classes': ('collapse',),
+            'fields': (
+                'subtitle', 'summary', 'block', 'redirect', 'keywords', 'itunes'
+            )
+        })
+    )
 
 admin.site.register(models.Channel, ChannelAdmin)
--- podcast/models.py
+++ podcast/models.py
@@ -11,21 +11,37 @@ class Channel(models.Model):
     language = models.CharField(max_length=5, blank=True)
     copyright = models.CharField(max_length=255, blank=True)
 
-    managing_editor = models.EmailField(blank=True)
-    web_master = models.EmailField(blank=True)
+    managing_editor = models.EmailField(
+        blank=True,
+        help_text='Email address for person responsible for editorial content.'
+    )
+    web_master = models.EmailField(
+        blank=True,
+        help_text='Email address for person responsible for technical issues.'
+    )
 
     pub_date = models.DateTimeField(blank=True, null=True)
     last_build_date = models.DateTimeField(blank=True, null=True)
 
+    ttl = models.PositiveIntegerField(
+        verbose_name='TTL',
+        blank=True,
+        null=True,
+        help_text='The number of minutes a channel can be cached before refreshing.',
+    )
+
+    generator = models.CharField(max_length=255, blank=True)
+
     # iTunes
     subtitle = models.CharField(max_length=255, blank=True)
     summary = models.TextField(blank=True)
-    # category = models.ManyToManyField(ChildCategory, related_name='show_categories', blank=True)
-    # explicit = models.CharField(max_length=255, default='No', choices=EXPLICIT_CHOICES, blank=True)
-    block = models.BooleanField(default=False,)
     redirect = models.URLField(blank=True)
     keywords = models.CharField(max_length=255, blank=True)
     itunes = models.URLField('iTunes Store URL', blank=True)
+    block = models.BooleanField(
+        default=False,
+        help_text='Block this podcast on iTunes.'
+    )
 
     def __str__(self):
         return self.title
